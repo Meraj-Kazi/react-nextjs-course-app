a266825b4d3f9054ab52e2a2c73a21eb0a50578b
meraj-kazi <kazimeraj11@gmail.com>
2021-07-26 20:05:08 +0600

    update

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,4 +1,10 @@
+import { useRouter } from 'next/router';
+
 function DetailPage () {
+    const router = useRouter();
+
+    console.log(router.query.newsId);
+
     return <h1>The Detail Page</h1>
 }
 
